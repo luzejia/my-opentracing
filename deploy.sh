8a2d1589d3a7a322ecd8012c5b27cee2d5409d5a
luzejia <52991866+luzejia@users.noreply.github.com>
2021-09-05 09:39:58 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,29 @@
 echo start to deploy
 
 cd service_a
-docker build -t uhub.service.ucloud.cn/luzejia/tracing-a:v0.0.12 .
-docker push uhub.service.ucloud.cn/luzejia/tracing-a:v0.0.12
+docker build -t iamges-address .
+docker iamges-address
 kubectl delete -f my-tracing.yaml
 kubectl apply -f my-tracing.yaml
 cd ..
 
 cd service_b
-docker build -t uhub.service.ucloud.cn/luzejia/tracing-b:v0.0.12 .
-docker push uhub.service.ucloud.cn/luzejia/tracing-b:v0.0.12
+docker build -t amges-address .
+docker push amges-address
 kubectl delete -f my-tracing.yaml
 kubectl apply -f my-tracing.yaml
 cd ..
 
 cd service_c
-docker build -t uhub.service.ucloud.cn/luzejia/tracing-c:v0.0.12 .
-docker push uhub.service.ucloud.cn/luzejia/tracing-c:v0.0.12
+docker build -t amges-address .
+docker push amges-address
+kubectl delete -f my-tracing.yaml
+kubectl apply -f my-tracing.yaml
+cd ..
+
+cd service_d
+docker build -t amges-address .
+docker push amges-address
 kubectl delete -f my-tracing.yaml
 kubectl apply -f my-tracing.yaml
 cd ..
